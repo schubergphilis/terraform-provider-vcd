3337ba4b3c33bc0a37aed76e454ae5a5156b1468
Tom Snuverink <tsnuverink@schubergphilis.com>
2026-02-05 11:56:52 +0100

    Add pr ci and rename to schubergphilis

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/vmware/terraform-provider-vcd/v3/vcd"
+	"github.com/schubergphilis/terraform-provider-vcd/v3/vcd"
 )
 
 func main() {
